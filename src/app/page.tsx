3d113b69c19d919eec5f4c4e5b91f596a7a3f341
Abner da Rocha Lourenço <abner2010_hm@hotmail.com>
2025-02-18 07:51:42 -0300

    feat: init project and components created

---
+++ src/app/page.tsx
@@ -0,0 +1,29 @@
+import Button from "@/components/button";
+import IconButton from "@/components/icon-button";
+import { InputField, InputIcon, InputRoot } from "@/components/input";
+import { ArrowRight,Copy, Mail } from 'lucide-react';
+
+export default function Home() {
+  return (
+    <main>
+      <Button>
+        Enviar
+        <ArrowRight/>
+      </Button>
+
+      <IconButton>
+        <Copy />
+      </IconButton>
+
+      <div>
+        <InputRoot error>
+          <InputField placeholder="Digite seu Email..."/>
+
+          <InputIcon>
+            <Mail className="size-5"/>
+          </InputIcon>
+        </InputRoot>
+      </div>
+    </main>
+  );
+}
