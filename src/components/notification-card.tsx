5962dbbbec6db65d75bdb3a06a2895441095895b
Abner da Rocha Lourenço <abner2010_hm@hotmail.com>
2025-02-22 21:10:31 -0300

    feat: add orval, integrate on backend to referrer links

--- src/components/notification-card.tsx
+++ src/components/notification-card.tsx
@@ -1,8 +1,7 @@
-'use client'
-
 import { Copy } from 'lucide-react'
+import type { ComponentProps } from 'react'
 
-interface NotificationCardProps {
+interface NotificationCardProps extends ComponentProps<'div'> {
   message: string
   visible: boolean
 }
@@ -15,10 +14,13 @@ export function showNotification(setVisible: (visible: boolean) => void) {
 export default function NotificationCard({
   message,
   visible,
+  ...props
 }: NotificationCardProps) {
   return (
     <div
-      className={`absolute flex items-center justify-between gap-2 flex-row bottom-12 right-12 max-w-[400px] p-5 rounded bg-blue shadow-lg transform transition-all duration-500 ease-in-out ${visible ? 'opacity-100 translate-y-0' : 'opacity-0 translate-y-8'}`}
+      data-visible={visible}
+      className="absolute flex items-center justify-between gap-2 flex-row bottom-12 right-12 max-w-[400px] p-5 rounded bg-blue shadow-lg transform transition-all duration-500 ease-in-out data-[visible=true]:opacity-100 data-[visible=true]:translate-y-0 data-[visible=false]:opacity-0 data-[visible=false]:translate-y-8"
+      {...props}
     >
       <Copy className="size-5 text-gray-900" />
       <p className="text-gray-800 font-heading font-semibold">{message}</p>
